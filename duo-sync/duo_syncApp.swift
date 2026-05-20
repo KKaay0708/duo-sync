4917753a494c02467e07fbdc3a021c7e5b1be58f
Kaitlyn <kaykaycsc@gmail.com>
2026-05-19 19:35:02 -0700

    Initial Commit

---
+++ duo-sync/duo_syncApp.swift
@@ -0,0 +1,17 @@
+//
+//  duo_syncApp.swift
+//  duo-sync
+//
+//  Created by Kaitlyn Kwan on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct duo_syncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ duo-syncTests/duo_syncTests.swift
@@ -0,0 +1,17 @@
+//
+//  duo_syncTests.swift
+//  duo-syncTests
+//
+//  Created by Kaitlyn Kwan on 5/19/26.
+//
+
+import Testing
+@testable import duo_sync
+
+struct duo_syncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
